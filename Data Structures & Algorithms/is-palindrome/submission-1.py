308ae40ac514c874d7efb0ebb8238a163d82d65b
Seren <76075626+xidoudou@users.noreply.github.com>
2026-09-07 17:26:12 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        letters = [letter.lower() for letter in s if letter.isalpha()]
+        left, right = 0, len(letters)-1
+        while left < right:
+            if letters[left] != letters[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
